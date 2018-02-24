6a4400a91dc264736e0dc745627de3027bbc3801
Nikolaos Rafail Nikolouzos <nick@nnikolouzos.xyz>
2018-02-24 20:29:50 +0200

    Build the UI for the application Added functionality for the ask button

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = (Button) findViewById(R.id.askButton);
+
+        final int[] eightBall = new int[] {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView ballImage = (ImageView) findViewById(R.id.ballImage);
+
+                int random;
+                Random randomNumberGen = new Random();
+                random = randomNumberGen.nextInt(5);
+
+                Log.d("eightBallApp", "The random number is : " + random);
+                ballImage.setImageResource(eightBall[random]);
+
+            }
+        });
     }
 }
